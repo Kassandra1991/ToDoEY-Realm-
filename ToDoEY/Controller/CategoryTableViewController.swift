74101d505564a06cd5427679be1d07fbaf3803a0
Alex Asichka <sanechka3@mail.ru>
2023-03-30 11:31:31 +0300

    add coredate for category

--- ToDoEY/Controller/CategoryTableViewController.swift
+++ ToDoEY/Controller/CategoryTableViewController.swift
@@ -6,28 +6,42 @@
 //
 
 import UIKit
+import CoreData
 
 class CategoryTableViewController: UITableViewController {
 
+    var categories = [CategoryItem]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationController?.navigationBar.backgroundColor =  #colorLiteral(red: 0.2103916407, green: 0.5888115764, blue: 1, alpha: 1)
+        //loadCategories()
+    }
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return categories.count
     }
-
-    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell", for: indexPath)
+        let category = categories[indexPath.row].name
+        return cell
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
+    
+    // MARK: - Manipulation
     
 }
